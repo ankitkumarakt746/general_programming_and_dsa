a74ef83be4394a5e399cc2eb9253b13c5ada89e0
Ankit <ankitkumarakt746@gmail.com>
2021-04-08 17:14:44 +0530

    commit 3

--- bash/hackersploit/ping_sweep.sh
+++ bash/hackersploit/ping_sweep.sh
@@ -2,13 +2,33 @@
 
 #Simple Ping sweep script
 
-read -p "Please enter the first 3 octets of the subnet: " SUBNET        #echo "Please enter the subnet: "
+LINES=""
+
+read -p "Please enter the first 3 octets of the subnet (e.g, 192.168.1): " SUBNET        #echo "Please enter the subnet: "
                                                                         #read SUBNET
 
-read -p "Please enter start IP: " START
-read -p "Please enter end IP: " END
+read -p "Please enter start IP (e.g, 56): " START
+read -p "Please enter end IP (e.g, 59): " END
+echo -e "\nStarting\n"
 for IP in $(seq $START $END );
 do
-	ping -c 1 $SUBNET.$IP
-	echo 
+	ping -c 1 $SUBNET.$IP > .temp
+
+	LINES=`wc -l .temp`
+
+	if [ ${LINES:0:1} = "5" ];
+	then
+		echo "$SUBNET.$IP -> Not responding"
+	else
+		echo "$SUBNET.$IP -> Responded with pong"
+	fi
 done
+
+rm -rf .temp
+
+
+
+
+#Proposed modification ideas:
+#Supress the ping output display on console and if recieved value is 1 in the resultant ping output
+#then show that it responded with pong else show not found 
