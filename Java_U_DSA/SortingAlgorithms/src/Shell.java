96a540a420822e9dae1a21d63ddad85cdd29fbc9
Ankit <ankitkumarakt746@gmail.com>
2021-05-12 22:10:10 +0530

    another commit

--- Java_U_DSA/SortingAlgorithms/src/Shell.java
+++ Java_U_DSA/SortingAlgorithms/src/Shell.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
-public class Insertion {
+public class Shell {
 
-    public static void insertionSort(int[] arr){
-        for(int i=0; i<arr.length; i++) {
-            int currElement = arr[i];
-            int j = i - 1;
+    public static void shellSort(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            int currEle = arr[i];
+            int j = i-1;
 
-            while (j >= 0 && arr[j] > currElement) {
-                arr[j + 1] = arr[j];
-                j--;
+            while(j>=0 && arr[j]>currEle){
+
             }
-            arr[j + 1] = currElement;
         }
     }
 
@@ -24,7 +22,7 @@ public class Insertion {
             arr[i] = sc.nextInt();
         }
 
-        insertionSort(arr);
+        shellSort(arr);
 
         for(int i=0; i<n; i++){
             System.out.print(arr[i] + " ");
